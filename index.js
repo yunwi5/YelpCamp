a30093c21eacfec6edc5cb18e42c28aad4afb66a
Yunkeun <1009jyk@gmail.com>
2022-03-04 23:53:35 +1300

    Adding authentication

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const ejsMate = require('ejs-mate') // For templating boilerplate code for ejs f
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
 
 const ExpressError = require('./utils/ExpressError')
 const campgroundsRoutes = require('./routes/campgrounds')
 const reivewsRoutes = require('./routes/reviews')
+const usersRoutes = require('./routes/users')
+const User = require('./models/user')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -32,8 +36,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
     resave: false,
@@ -47,6 +49,15 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+// PASSPORT
+app.use(passport.initialize());
+// This should be after session
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 // Middleware for flashing message to the user without passing them everytime manually.
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
@@ -57,6 +68,7 @@ app.use((req, res, next) => {
 
 app.use('/campgrounds', campgroundsRoutes);
 app.use('/campgrounds/:id/reviews', reivewsRoutes);
+app.use('/', usersRoutes);
 
 app.get('/', (req, res) => {
     res.render('home')
